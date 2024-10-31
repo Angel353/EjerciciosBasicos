d5bb932d6857acf1024dd58f359e737d09378802
Angel353 <a01735880@tec.mx>
2024-10-31 16:36:06 -0600

    Código Inicial

---
+++ Ejercicios.py
@@ -0,0 +1,12 @@
+print("Este proyecto tiene por objetivo generar operaciones matemáticas simples")
+
+a = float(input("Dame un número: "))
+b = float(input("Dame otro número: "))
+
+print(a)
+print(b)
+
+def suma():
+    return a + b
+    
+print("Esta es el resultado de sumar los números que colocaste: ", suma())
